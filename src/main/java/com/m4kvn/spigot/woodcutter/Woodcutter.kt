bb8a43178c02060a5da7608d4d33be8f0a33a6e6
m4kvn <fun.masahiro@gmail.com>
2021-05-10 20:38:24 +0900

    Configure Kotlin

--- src/main/java/com/m4kvn/spigot/woodcutter/Woodcutter.kt
+++ src/main/java/com/m4kvn/spigot/woodcutter/Woodcutter.kt
@@ -1,11 +1,23 @@
 package com.m4kvn.spigot.woodcutter
 
+import org.bukkit.Bukkit
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class Woodcutter : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.pluginManager.registerEvents(object : Listener {
+            @EventHandler(priority = EventPriority.MONITOR)
+            fun onBlockBreak(event: BlockBreakEvent) {
+                if (event.isCancelled) return
+                Bukkit.broadcastMessage("event=${event.block.blockData.asString}")
+            }
+        }, this)
     }
 
     override fun onDisable() {
