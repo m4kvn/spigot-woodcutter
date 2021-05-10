d1318ec03dd1eff76078e121864bd28987ada1dd
m4kvn <fun.masahiro@gmail.com>
2021-05-10 19:53:56 +0900

    Initialize repository

---
+++ src/main/java/com/m4kvn/spigot/woodcutter/Woodcutter.kt
@@ -0,0 +1,14 @@
+package com.m4kvn.spigot.woodcutter
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Woodcutter : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
