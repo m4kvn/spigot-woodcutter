43c6a6c0b6427df750d9ad81eb93110aefec18ae
m4kvn <makun@pixiv.com>
2021-05-11 07:28:32 +0900

    Implemented breaking item in main hand

--- src/main/java/com/m4kvn/spigot/woodcutter/Woodcutter.kt
+++ src/main/java/com/m4kvn/spigot/woodcutter/Woodcutter.kt
@@ -1,13 +1,20 @@
 package com.m4kvn.spigot.woodcutter
 
 import org.bukkit.Material
+import org.bukkit.Sound
+import org.bukkit.Statistic
 import org.bukkit.block.Block
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.Damageable
+import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.plugin.java.JavaPlugin
+import kotlin.random.Random
 
 class Woodcutter : JavaPlugin(), Listener {
 
@@ -35,6 +42,14 @@ class Woodcutter : JavaPlugin(), Listener {
         checkedBlocks.forEach {
             it.breakNaturally(itemInMainHand)
         }
+
+        val isBroken = itemInMainHand.damage(checkedBlocks.size)
+        if (isBroken) {
+            event.player.breakItemInMainHand()
+        }
+
+        event.player.incrementStatistic(Statistic.MINE_BLOCK, event.block.type, checkedBlocks.size)
+        event.player.incrementStatistic(Statistic.USE_ITEM, itemInMainHand.type, checkedBlocks.size)
     }
 
     override fun onEnable() {
@@ -42,6 +57,31 @@ class Woodcutter : JavaPlugin(), Listener {
     }
 
     override fun onDisable() {}
+
+    private fun Player.breakItemInMainHand() {
+        world.playSound(location, Sound.ENTITY_ITEM_BREAK, 1f, 1f)
+        inventory.setItemInMainHand(ItemStack(Material.AIR))
+    }
+
+    private fun ItemStack.calcDamage(amount: Int): Int {
+        var damage = amount
+        if (containsEnchantment(Enchantment.DURABILITY)) {
+            val level = getEnchantmentLevel(Enchantment.DURABILITY)
+            repeat(amount) {
+                damage -= if (Random.nextInt(level) == 0) 0 else 1
+            }
+        }
+        return damage
+    }
+
+    private fun ItemStack.damage(amount: Int): Boolean {
+        val damageable = itemMeta as? Damageable ?: return false
+        if ((damageable as ItemMeta).isUnbreakable) return false
+        val damage = calcDamage(amount)
+        damageable.damage = damageable.damage + damage
+        itemMeta = damageable
+        return damageable.damage >= type.maxDurability
+    }
 
     private fun Block.isSameLog(block: Block): Boolean =
         block.blockData.material == blockData.material
@@ -80,8 +120,4 @@ class Woodcutter : JavaPlugin(), Listener {
             Material.WOODEN_AXE -> true
             else -> false
         }
-
-    companion object {
-        private const val METADATA_KEY = "woodcutter"
-    }
 }
