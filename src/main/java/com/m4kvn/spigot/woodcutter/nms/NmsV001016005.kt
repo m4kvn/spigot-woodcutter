511a2c53a7c46988cfd9d60d0212a743450d8bc7
m4kvn <makun@pixiv.com>
2021-05-12 03:24:35 +0900

    Changed to be able to immediately acquire items dropped by leaves

--- src/main/java/com/m4kvn/spigot/woodcutter/nms/NmsV001016005.kt
+++ src/main/java/com/m4kvn/spigot/woodcutter/nms/NmsV001016005.kt
@@ -1,8 +1,6 @@
 package com.m4kvn.spigot.woodcutter.nms
 
-import net.minecraft.server.v1_16_R3.BlockPosition
-import net.minecraft.server.v1_16_R3.Blocks
-import net.minecraft.server.v1_16_R3.ItemStack
+import net.minecraft.server.v1_16_R3.*
 import org.bukkit.block.Block
 import org.bukkit.craftbukkit.v1_16_R3.block.CraftBlock
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer
@@ -19,7 +17,27 @@ class NmsV001016005 : NMS {
         val tileEntity = world.getTileEntity(craftBlock.position)
         val iBlockData = craftBlock.nms
         if (iBlockData.block != Blocks.AIR && (!iBlockData.isRequiresSpecialTool)) {
-            NMSBlock.dropItems(craftBlock.nms, world, dropPosition, tileEntity, null, ItemStack.b)
+            val drops = NMSBlock.getDrops(iBlockData, world, dropPosition, tileEntity, null, ItemStack.b)
+            drops.forEach { itemStack ->
+                val d0 = (world.random.nextFloat() * 0.5f).toDouble() + 0.25
+                val d1 = (world.random.nextFloat() * 0.5f).toDouble() + 0.25
+                val d2 = (world.random.nextFloat() * 0.5f).toDouble() + 0.25
+                if (!world.isClientSide && !itemStack.isEmpty && world.gameRules.getBoolean(GameRules.DO_TILE_DROPS)) {
+                    val entityItem = EntityItem(world,
+                        dropPosition.x + d0,
+                        dropPosition.y + d1,
+                        dropPosition.z + d2,
+                        itemStack
+                    )
+                    entityItem.n()
+                    if (world.captureDrops != null) {
+                        world.captureDrops.add(entityItem)
+                    } else {
+                        world.addEntity(entityItem)
+                    }
+                }
+            }
+            iBlockData.dropNaturally(world, dropPosition, ItemStack.b)
         }
         craftBlock.setTypeAndData(Blocks.AIR.blockData, true)
     }
